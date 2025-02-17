d26b7170906713c1ee5fb24f3d8d032da09ab4d6
JaeMoon <ljm96040@gmail.com>
2025-02-17 09:53:39 +0900

    v0.3 day06 is_even

--- day06.py
+++ day06.py
@@ -4,15 +4,20 @@ def is_even(n) -> bool:
     :param n: 판정할 정수
     :return: 짝수면 True, 홀수면 False
     """
-    if n % 2 == 0:
-        return True
-    return False
+    return not n & 1
+#비트연산을 통해 코드를 줄일수있음
+
+   # if n % 2 == 0:
+   #      return True
+   #  return False
+
+n = int(input())
+print(is_even(n))
+
 
 a = 10
 b = 11
 # bit operation (2진수로 바꾼후 and or nor로 연산)
 print (a & b) #10
 print (a | b) #11
-print (a ^ b) #1
-n = int(input())
-print(is_even)
+print (a ^ b) #11
