19ff3c69b55c40c4ebfebd640a4240ec9b3ac681
JaeMoon <ljm96040@gmail.com>
2025-02-17 11:36:38 +0900

    v0.5 day06 O(n)

--- day06.py
+++ day06.py
@@ -1,53 +1,11 @@
-# repetition(반복문)
-# def factorial(number) -> int:
-#     '''
-#     factorial by repetition
-#     :param number: number (int)
-#     :return: factorial result (int)
-#     '''
-#     result = 1
-#     for i in range(1, number+1):
-#         result = result * i
-#     return result
+# O(n)
+n = int(input())
+r = 0
+for i in range(n+1):
+    r = r + i
 
-#recursion(재귀함수)
-def factorial(number) -> int:
-    '''
-    factorial by recursion
-    :param number: number (int)
-    :return: factorial result (int)
-    '''
-    if number == 1:
-        return 1
-    else:
-        return number * factorial(number - 1)
+print(r)
 
 
-def nCr(n, r) -> int:
-    '''
-    조합 함수
-    :param n:
-    :param r:
-    :return:
-    '''
-    numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(r)
-    return int(numerator / denominator)
 
-
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n, r)}")
-    # f = int(input())
-    # print(factorial(f))
-
-def dec_oct(n) -> int:
-    if n == 0:
-        return ""
-    else:
-        return dec_oct(n//8) + str(n%8)
-
-    n = int(input())
-    print(dec_oct(n))
-
+# *OCP : 수정에는 닫혀있고 확정에는 열려있음 -> 어떻게? 클로져를 이용한 데코레이터를 사용하여
