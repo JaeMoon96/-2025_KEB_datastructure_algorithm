dd1443a666356af6b8edc958f3606e7c27d757a7
JaeMoon <ljm96040@gmail.com>
2025-02-20 10:23:37 +0900

    v3.51 day08 assingment

--- day08.py
+++ day08.py
@@ -27,13 +27,13 @@ def find_min(node):
         node = node.left
     return node
 
-def delete(root, value2):
+def delete(root, value):
     if root == None or root.data == None:
         return root
-    if value2 < root.data:
-        root.left = delete(root.left, value2)
-    elif value2 > root.data:
-        root.right = delete(root.right, value2)
+    if value < root.data:
+        root.left = delete(root.left, value)
+    elif value > root.data:
+        root.right = delete(root.right, value)
     else:
         if root.left == None and root.right == None:
             return None
@@ -47,6 +47,16 @@ def delete(root, value2):
 
     return root
 
+def search(root, value):
+    if root is None or root.data is None:
+        return None
+
+    if value == root.data:
+        return root
+    elif value < root.data:
+        return search(root.left, value)
+    else:
+        return search(root.right, value)
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9]
     root = None
@@ -73,17 +83,6 @@ if __name__ == "__main__":
 
     print("BST 구성 완료")
 
-
-def search(root, value3):
-    if root is None or root.data is None:
-        return None
-
-    if value3 == root.data:
-        return root
-    elif value3 < root.data:
-        return search(root.left, value3)
-    else:
-        return search(root.right, value3)
 
 while True:
         menu = input("추가(A), 삭제(D), 검색(S), 종료(Q)를 선택하세요: ")
