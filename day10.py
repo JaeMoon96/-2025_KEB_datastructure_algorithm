933019002c0d3b963f4a33d37fefd13513f3d18c
JaeMoon <ljm96040@gmail.com>
2025-02-21 13:45:22 +0900

    v5.2 day10 sort

--- day10.py
+++ day10.py
@@ -36,23 +36,28 @@ def bubble_sort(l):
             return l
     return l
 
-
+#중복 제거문제 수정
 def quick_sort(l):
     n = len(l)
     if n<=1: return l
     pivot = l[n//2]
-    left, right = list(), list()
+    left, mid, right = list(), list(), list()
 
     for i in l:
         if i < pivot:
             left.append(i)
         elif i > pivot:
             right.append(i)
+        else:
+            mid.append(i)
 
-    return quick_sort(left)+[pivot]+quick_sort(right)
+    return quick_sort(left)+mid+quick_sort(right)
 
 
-lists1 = [random.randint(1, 100000) for _ in range(100000)]
+# lists = [4, 55, 55, 7, 55, 9, 11]
+# print(quick_sort(lists))
+
+lists1 = [random.randint(1, 100000) for _ in range(10000)]
 lists2 = lists1.copy()
 lists3 = lists1.copy()
 bubble_sort(lists1)
